f1ab19258798046cc8f83ae00fdbfa576ebe5bf7
Angelica-Solis <masolissi@est.utn.ac.cr>
2026-08-19 13:14:22 -0600

    Configuracion inicial del lab de estadistica

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="container mt-5">
+            <h1>Laboratorio de Estadística</h1>
+
+            <p>
+                Análisis estadístico de los datos de la población.
+            </p>
+        </div>
+    );
+}
+
+export default App;
